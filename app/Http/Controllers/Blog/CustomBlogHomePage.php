c252414fe563acc423525b8b8aa4f3a6d7d00c1d
denissc <capslockk3@gmail.com>
2020-04-15 03:54:26 +0300

    rm unused

--- app/Http/Controllers/Blog/CustomBlogHomePage.php
+++ app/Http/Controllers/Blog/CustomBlogHomePage.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers\Blog;
 
-//use App\Http\Controllers\Controller;
-use Bjuppa\LaravelBlog\Contracts\BlogRegistry;
 use Bjuppa\LaravelBlog\Eloquent\BlogEntry;
 use Bjuppa\LaravelBlog\Http\Controllers\BaseBlogController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
 class CustomBlogHomePage extends BaseBlogController
